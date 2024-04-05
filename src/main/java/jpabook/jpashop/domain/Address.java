ee5025fd1ea3fb0235c28a4a18c391febdffcaa3
donew <eastgloss0330@naver.com>
2024-04-05 15:33:55 +0900

    [Refactor] Address 값 타입 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    // 길이 등 공통 rule을 관리하기 쉽다
+    @Column(length = 10)
+    private String city;
+    @Column(length = 20)
+    private String street;
+    @Column(length = 5)
+    private String zipcode;
+
+    // 의미 있는 비즈니스 로직 메소드를 만들 수 있다
+    public String fullAddress() {
+        return getCity() + " " + getStreet() + " " + getZipcode();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
